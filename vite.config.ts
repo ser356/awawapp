f8eb71c385b7b3d58592b8e111d3461624c49d44
ser356 <sergiogarciaseco@usal.es>
2026-04-04 00:14:28 +0200

    feat: add alias configuration for vue-i18n in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [vue()],
 
+  resolve: {
+    alias: {
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
+    },
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
